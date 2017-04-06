6576c2b4e1c338469ff7cd1fef1a62329925fbf1
jaysonbourne <julie.suhyoung.kim@gmail.com>
2017-04-05 21:54:01 -0700

    Delete makingDict.py

--- makingDict.py
+++
@@ -1,7 +0,0 @@
-#def make_dict(name, favorite_animal)
-name = ["Anna", "Eli", "Pariece", "Brendan", "Amy", "Shane", "Oscar"]
-favorite_animal = ["horse", "cat", "spider", "giraffe", "ticks", "dolphins", "llamas"]
-print name, favorite_animal
-    #new_dict = {}
-namestoanimals = zip(name, favorite_animal)
-print namestoanimals
