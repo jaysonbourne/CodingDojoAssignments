a588ce5863aa668d346fddc39e382cc81dafee35
jaysonbourne <julie.suhyoung.kim@gmail.com>
2017-04-05 22:15:29 -0700

    mathdojo update
    couldn't figure out how to update file...too tired.

--- python_time_time/math_dojo.py
+++ python_time_time/math_dojo.py
@@ -1,3 +1,4 @@
+"""
 class MathDojo(object):
 
     def __init__(self):
@@ -21,3 +22,50 @@ class MathDojo(object):
             else:
                 self.result -= arg
         return self
+"""
+"""
+class MathDojo(object):
+	def __init__(self):
+		self.result = 0
+		self.sum = 0
+		self.sub = 0
+		print "Let's do math!"
+	def add(self, *nums):
+		if type(nums) is tuple:
+			for num in nums:
+				self.result += num
+		return self
+	def subtract(self, *nums):
+		if type(nums) is tuple:
+			for num in nums:
+				self.result -= num
+		return self
+
+md = MathDojo()
+
+print md.add(2).add(2, 5).subtract(3, 2).result
+"""
+class MathDojo(object):
+	def __init__(self):
+		self.result = 0
+		print "Let's do math!"
+	def add(self, *nums):
+		for item in nums:
+			if type(item) is list:
+				for num in item:
+					self.result += num
+			else:
+				self.result += item
+		return self
+	def subtract(self, *nums):
+		for item in nums:
+			if type(item) is list:
+				for num in item:
+					self.result -= num
+			else:
+				self.result -= item
+		return self
+
+md = MathDojo()
+
+print md.add([1],3,4).add([3, 5, 7, 8], [2, 4.3, 1.25]).subtract(2, [2,3], [1.1, 2.3]).result
